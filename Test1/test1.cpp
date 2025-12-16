b7fb61f34ba2a0e65af694ace1cc228a4ddec058
RotiCanaiMamak <ivantong1122@gmail.com>
2025-12-16 20:12:35 +0800

    drew some head things, added metal.bmp

--- Test1/test1.cpp
+++ Test1/test1.cpp
@@ -26,7 +26,7 @@ float translatex = 0, translatey = 0, translatez = 0;
 float translateinc = 0.1;
 bool changeView = false;
 
-GLuint waterTex = 0;
+GLuint metalTex = 0;
 
 BITMAP BMP;
 HBITMAP hBMP = NULL;
@@ -331,7 +331,7 @@ void drawTriangle3D(float r, float g, float b, float x1, float y1, float z1, flo
 	glVertex3f(x2, y2, z2);
 	glVertex3f(x3, y3, z3);
 	glEnd();
-}	
+}
 
 void drawLine3D(float r, float g, float b, float x1, float y1, float z1, float x2, float y2, float z2, float thick) {
 	glLineWidth(thick);
@@ -390,7 +390,7 @@ void drawCylinder(float r, float g, float b, float x, float y, float z, float ba
 	glPopMatrix();
 }
 
-void drawDonut(float r, float g, float b, float x, float y, float z, float majorRadius, float minorRadius, int segments, float arcAngle) {
+void drawDonut(float r, float g, float b, float majorRadius, float minorRadius, float arcAngle, GLuint texture) {
 	//majorRadius: distance from center of torus to center of tube
 	//minorRadius: radius of the tube
 	//segments: number of segments around the major circle
@@ -403,13 +403,17 @@ void drawDonut(float r, float g, float b, float x, float y, float z, float major
 
 	//convert arcAngle to radians and calculate how many segments to draw
 	float arcRadians = (arcAngle / 180.0f) * PI; //convert angle to radian (total angle to draw)
-	float angleStep = arcRadians / segments; //angle to turn for the next segment
+	float angleStep = arcRadians / 40; //angle to turn for the next segment
 
 	int tubeSegments = 20; //number of segments around the tube cross section (higher means rounder)
 
-	for (int i = 0; i < segments; i++) { //draw 1 segment between angle1 and angle2
+	for (int i = 0; i < 40; i++) { //draw 1 segment between angle1 and angle2
 		float angle1 = i * angleStep;
 		float angle2 = (i + 1) * angleStep;
+
+		//texture coordinate for major circle (0 to 1 along the donut)
+		float u1 = (float)i / 40;
+		float u2 = (float)(i + 1) / 40;
 
 		glBegin(GL_QUAD_STRIP);
 		for (int j = 0; j <= tubeSegments; j++) { //draw tube cross section to make it rounder
@@ -426,6 +430,9 @@ void drawDonut(float r, float g, float b, float x, float y, float z, float major
 			*/
 			float tubeAngle = (j * 2 * PI) / tubeSegments; //calculate which face we are currently drawing
 
+			//texture coordinate for tube cross section (0 to 1 around the tube)
+			float v = (float)j / tubeSegments;
+
 			//first ring
 			float x1 = (majorRadius + minorRadius * cos(tubeAngle)) * cos(angle1);
 			float y1 = minorRadius * sin(tubeAngle);
@@ -436,7 +443,7 @@ void drawDonut(float r, float g, float b, float x, float y, float z, float major
 			float ny1 = sin(tubeAngle);
 			float nz1 = cos(tubeAngle) * sin(angle1);
 			glNormal3f(nx1, ny1, nz1);
-			glVertex3f(x + x1, y + y1, z + z1);
+			glVertex3f(x1, y1, z1);
 
 			//second ring
 			float x2 = (majorRadius + minorRadius * cos(tubeAngle)) * cos(angle2);
@@ -448,10 +455,163 @@ void drawDonut(float r, float g, float b, float x, float y, float z, float major
 			float ny2 = sin(tubeAngle);
 			float nz2 = cos(tubeAngle) * sin(angle2);
 			glNormal3f(nx2, ny2, nz2);
-			glVertex3f(x + x2, y + y2, z + z2);
+			glVertex3f(x2, y2, z2);
 		}
 		glEnd();
 	}
+}
+
+void drawDonutOutline(float r, float g, float b, float majorRadius, float minorRadius, int numCircles, float arcAngle, float lineThickness) {
+	//majorRadius: distance from center of torus to center of tube
+	//minorRadius: radius of the tube
+	//numCircles: number of vertical circles to draw around the donut
+	//arcAngle: angle in degrees (360 = full donut, 180 = half, 90 = quarter)
+
+	glColor3f(r, g, b);
+	GLfloat col[] = { r, g, b };
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, col);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, col);
+	glLineWidth(lineThickness);
+
+	float arcRadians = (arcAngle / 180.0f) * PI; //convert arcAngle to radians
+	float realNumCircles = (arcAngle / 360) * numCircles; //calculate how many lines to draw based on the arcangle
+	float angleStep = arcRadians / (realNumCircles - 1); //divide by numCircles-1 to include both start and end
+
+	int tubeSegments = 50; //number of points to create smooth circle
+
+	//draw vertical circles at evenly spaced positions
+	for (int i = 0; i < realNumCircles; i++) {
+		float majorAngle = i * angleStep;
+
+		glBegin(GL_LINE_LOOP);
+		for (int j = 0; j < tubeSegments; j++) {
+			float tubeAngle = (j * 2 * PI) / tubeSegments;
+
+			//calculate position on the vertical circle
+			float xPos = (majorRadius + minorRadius * cos(tubeAngle)) * cos(majorAngle);
+			float yPos = minorRadius * sin(tubeAngle);
+			float zPos = (majorRadius + minorRadius * cos(tubeAngle)) * sin(majorAngle);
+
+			glVertex3f(xPos, yPos, zPos);
+		}
+		glEnd();
+	}
+}
+
+void drawMetalStrips(float r, float g, float b, float majorRadius, float minorRadius, float arcAngle, GLuint texture) {
+	drawDonut(r, g, b, majorRadius, minorRadius, arcAngle, metalTex);
+	drawDonutOutline(0, 0, 0, majorRadius, minorRadius, 40, arcAngle, 1);
+}
+
+void drawQuadMetalStrips(int x, int y, int z) {
+	glPushMatrix();
+	glScaled(0.8, 0.9, 0.8);
+	glRotated(95 * y, 0, 0, 1);
+	glRotated(0, 0, 1, 0);
+	glRotated(-18 * x, 1, 0, 0);
+	drawMetalStrips(0.7, 0.7, 0.7, 0.5, 0.1, 90, metalTex);
+	glPopMatrix();
+
+	glPushMatrix();
+	glScaled(0.8, 0.9, 0.8);
+	glRotated(95 * y, 0, 0, 1);
+	glRotated(35, 0, 1, 0);
+	glRotated(-36 * x, 1, 0, 0);
+	drawMetalStrips(0.7, 0.7, 0.7, 0.5, 0.1, 120, metalTex);
+	glPopMatrix();
+
+	glPushMatrix();
+	glScaled(0.8, 0.9, 0.8);
+	glRotated(95 * y, 0, 0, 1);
+	glRotated(60, 0, 1, 0);
+	glRotated(-52 * x, 1, 0, 0);
+	drawMetalStrips(0.7, 0.7, 0.7, 0.5, 0.1, 120, metalTex);
+	glPopMatrix();
+
+	glPushMatrix();
+	glScaled(0.8, 0.9, 0.8);
+	glRotated(95 * y, 0, 0, 1);
+	glRotated(60, 0, 1, 0);
+	glRotated(-75 * x, 1, 0, 0);
+	drawMetalStrips(0.7, 0.7, 0.7, 0.5, 0.1, 110, metalTex);
+	glPopMatrix();
+
+	glPushMatrix();
+	glScaled(0.8, 0.9, 0.8);
+	glRotated(95 * y, 0, 0, 1);
+	glRotated(70, 0, 1, 0);
+	glRotated(-90 * x, 1, 0, 0);
+	drawMetalStrips(0.7, 0.7, 0.7, 0.5, 0.1, 70, metalTex);
+	glPopMatrix();
+
+}
+
+void drawSphereMetalStrips() {
+	//center (vertical)
+	glPushMatrix();
+	glRotated(90, 0, 0, 1);
+	drawMetalStrips(0.7, 0.7, 0.7, 0.5, 0.1, 360, metalTex);
+	glPopMatrix();
+
+	//center (horizontal)
+	glPushMatrix();
+	glScaled(1, 1, 0.9);
+	drawMetalStrips(0.7, 0.7, 0.7, 0.5, 0.1, 360, metalTex);
+	glPopMatrix();
+
+	//top left
+	drawQuadMetalStrips(1, 1, 1);
+
+	//top right
+	drawQuadMetalStrips(-1, 1, 1);
+
+	//bot left
+	drawQuadMetalStrips(-1, -1, 1);
+
+	//bot right
+	drawQuadMetalStrips(1, -1, 1);
+}
+
+void drawEye() {
+	//left eye
+	//eyebrowns
+	glPushMatrix();
+	glTranslated(-0.3, -0.1, 0.4);
+	glScaled(1, 1, 2);
+	glRotated(-90, 1, 0, 0);
+	drawMetalStrips(0.7, 0.7, 0.7, 0.3, 0.05, 180, metalTex);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslated(-0.3, -0.1, 0.4);
+	glScaled(1, 1, 2);
+	glRotated(90, 1, 0, 0);
+	drawMetalStrips(0.7, 0.7, 0.7, 0.3, 0.05, 180, metalTex);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslated(-0.3, -0.1, 0.4);
+	drawSphere(1, 1, 1, 1, 0, 0, 0, 0.25, metalTex);
+	glPopMatrix();
+
+	//right eye
+	//eyebrowns
+	glPushMatrix();
+	glTranslated(0.3, -0.1, 0.4);
+	glScaled(1, 1, 2);
+	glRotated(-90, 1, 0, 0);
+	drawMetalStrips(0.7, 0.7, 0.7, 0.3, 0.05, 180, metalTex);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslated(0.3, -0.1, 0.4);
+	glScaled(1, 1, 2);
+	glRotated(90, 1, 0, 0);
+	drawMetalStrips(0.7, 0.7, 0.7, 0.3, 0.05, 180, metalTex);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslated(0.3, -0.1, 0.4);
+	drawSphere(1, 1, 1, 1, 0, 0, 0, 0.25, metalTex);
+	glPopMatrix();
 }
 
 void manageRotations() {
@@ -507,15 +667,37 @@ void display()
 	// ==========================
 
 	switch (renderNum) {
-	//ZC
+		//ZC
 	case 1:
 		manageRotations();
-		drawDonut(1, 0, 0, 0, 0, 0, 0.5, 0.1, 40, 360);
+
+		//head
+		drawSphereMetalStrips();
+
+		glPushMatrix();
+		glTranslated(0, -0.8, -0.3);
+		drawSphereMetalStrips();
+		glPopMatrix();
+
+		glPushMatrix();
+		glScaled(1, 1.1, 1);
+		glTranslated(-0.3, -0.5, -0.2);
+		drawSphereMetalStrips();
+		glPopMatrix();
+
+		glPushMatrix();
+		glScaled(1, 1.1, 1);
+		glTranslated(0.3, -0.5, -0.2);
+		drawSphereMetalStrips();
+		glPopMatrix();
+
+		//eye section
+		drawEye();
 
 		break;
 
 	case 2: {
-	
+
 	}
 
 
@@ -580,7 +762,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int nCmdShow)
 	gluQuadricNormals(cone, GLU_SMOOTH);
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
 
-	LoadBMPTexture("water.bmp", waterTex);
+	LoadBMPTexture("metal.bmp", metalTex);
 
 	glEnable(GL_TEXTURE_2D);
 
